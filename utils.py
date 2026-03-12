1f917f36147ffb1c346c5fe6a63067fe8cce0b7e
tjs-220 <tjs220@lehigh.edu>
2026-03-12 12:51:03 -0400

    utils

---
+++ utils.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+DSCI441 Project
+
+utils
+
+Taylor Schultz
+"""
+
+import time
+import psutil
+import os
+
+
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        print(f"Execution time: {end - start:.2f} seconds")
+        return result
+    return wrapper
+
+
+def memory_usage():
+    process = psutil.Process(os.getpid())
+    mem = process.memory_info().rss / (1024 ** 2)
+    print(f"Memory usage: {mem:.2f} MB")
